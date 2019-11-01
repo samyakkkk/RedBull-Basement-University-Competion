627cb3bdb597f654191b7e8f1a514b9b587d431c
samyakkkk <56000318+samyakkkk@users.noreply.github.com>
2019-11-01 01:36:00 -0700

    Update Create_account.py

--- Create_account.py
+++ Create_account.py
@@ -10,51 +10,57 @@ class redbull:
 
     def create_account(self):
         driver= webdriver.Firefox()
-        driver.get('http://www.fakemailgenerator.com/#/gustr.com')
-        time.sleep(2)
-        input_area = driver.find_element_by_xpath("//input[@id='home-email']")
-        input_area.clear()
-        input_area.send_keys(i + j +"")
-        copy_button = driver.find_element_by_xpath("//button[@id='copy-button']")
-        copy_button.click()
-        time.sleep(1)
-        driver.get("https://www.redbull.com/in-en/projects/red-bull-basement-university/project/1608")
-        time.sleep(30)
-        driver.find_element_by_xpath("//a[@class='rb-cookie-banner__btn']").click()
-        time.sleep(1)
-        for k in (1,4):
-            try:
-                login_button = driver.find_element_by_xpath("//div[@class='button button--red button__login display display--logged-out']")
-                time.sleep(1)
-            except Exception:
-                continue
+        try:
+            driver.get('http://www.fakemailgenerator.com/#/gustr.com')
+            time.sleep(2)
+            input_area = driver.find_element_by_xpath("//input[@id='home-email']")
+            input_area.clear()
+            input_area.send_keys(i + j + "")
+            copy_button = driver.find_element_by_xpath("//button[@id='copy-button']")
+            copy_button.click()
+            time.sleep(1)
+            driver.get("https://www.redbull.com/in-en/projects/red-bull-basement-university/project/1608")
+            time.sleep(30)
+            driver.find_element_by_xpath("//a[@class='rb-cookie-banner__btn']").click()
+            time.sleep(1)
+            for k in (1, 4):
+                try:
+                    login_button = driver.find_element_by_xpath(
+                        "//div[@class='button button--red button__login display display--logged-out']")
+                    time.sleep(1)
+                except Exception:
+                    continue
 
-        login_button.click()
-        time.sleep(8)
-        button_email = driver.find_element_by_xpath("//button[@class='btn-text']")
-        button_email.click()
-        redbull_email = driver.find_element_by_xpath("//input[@id='email']")
-        redbull_email.send_keys(i + j + "@gustr.com")
-        button_continue = driver.find_element_by_xpath("//button[@type='submit']")
-        button_continue.click()
-        time.sleep(10)
-        first_name = driver.find_element_by_xpath("//input[@id='firstName']")
-        last_name = driver.find_element_by_xpath("//input[@id='lastName']")
-        password = driver.find_element_by_xpath("//input[@id='password']")
-        first_name.send_keys(i)
-        last_name.send_keys(j)
-        password.send_keys('wings')
-        giughngub = driver.find_element_by_xpath("//button[@type='submit']")
-        giughngub.click()
-        time.sleep(10)
+            login_button.click()
+            time.sleep(8)
+            button_email = driver.find_element_by_xpath("//button[@class='btn-text']")
+            button_email.click()
+            redbull_email = driver.find_element_by_xpath("//input[@id='email']")
+            redbull_email.send_keys(i + j + "@gustr.com")
+            button_continue = driver.find_element_by_xpath("//button[@type='submit']")
+            button_continue.click()
+            time.sleep(10)
+            first_name = driver.find_element_by_xpath("//input[@id='firstName']")
+            last_name = driver.find_element_by_xpath("//input[@id='lastName']")
+            password = driver.find_element_by_xpath("//input[@id='password']")
+            first_name.send_keys(i)
+            last_name.send_keys(j)
+            password.send_keys('wings')
+            giughngub = driver.find_element_by_xpath("//button[@type='submit']")
+            giughngub.click()
+            time.sleep(10)
+        except Exception:
+            continue
+        
         driver.close()
 
 
 
 
 renn = redbull()
-f_name = [] #add first names here
-l_name = [] #add last names here
+# renn.open_browser()
+f_name = ['sheela', 'aastha']
+l_name = ['kumawat', 'shethi']
 for i,j in zip(f_name,l_name):
     renn.create_account()
     time.sleep(2)
